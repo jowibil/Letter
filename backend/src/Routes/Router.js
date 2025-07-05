ebc912fdb2ce214734b6484aaf42bdaa69a3f0b4
John Kenneth Pantonial <jkennn.02@gmail.com>
2025-07-05 23:20:16 +0800

    1

--- backend/src/Routes/Router.js
+++ backend/src/Routes/Router.js
@@ -1,9 +1,12 @@
 import express from 'express';
 import { getSomthng } from '../controllers/Controller.js'
+import { register } from '../controllers/Controller.js';
 
 const router=express.Router();
 
 router.get("/Login",getSomthng
 );
+router.get("/Register",register);
 
+router.get("/")
 export default router;
--- backend/src/controllers/Controller.js
+++ backend/src/controllers/Controller.js
@@ -1,3 +1,11 @@
 export const getSomthng=(req,res)=>{
 
-    res.status(200).send("This is the Login");}
+    res.status(200).send("This is the Login");
+};
+
+export  const register=async (req,res)=>{
+
+    res.status(200).send("This is register")
+}
+
+
