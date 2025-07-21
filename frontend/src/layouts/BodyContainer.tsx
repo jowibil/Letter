82bece1e812fb579c46bdaf954d3c79bf42f3927
jowibil <joebelegenota00@gmail.com>
2025-07-21 22:07:30 +0800

    Landing Changes

--- frontend/src/layouts/BodyContainer.tsx
+++ frontend/src/layouts/BodyContainer.tsx
@@ -1,23 +1,25 @@
 import { Link } from "react-router-dom";
-
+import graphic from "../assets/Graphic1.png"
+import graphic2 from "../assets/Graohic2.png"
 
 export function UpperContainer() {
     return (
         <div>
-            <div className="grid grid-cols-1 p-2 lg:grid-cols-2 md:grid-cols-2">
-                <div className="w-2xs h-64 p-2 m-auto">
-                </div>
-                <div className="grid grid-cols-1 gap-4 text-center p-2">
-                    <h3 className="scroll-m-20 text-center text-3xl m-2 font-bold tracking-tight text-balance lg:text-4xl">
+            <div className="grid grid-cols-1 gap-4 p-2 md:grid-cols-2">
+                    <div className="p-2">
+                    <img src={graphic} alt="Learn to Code Image" className="w-xs lg:w-xl" />
+                    </div>
+                <div className="flex flex-col gap-4 mt-4 md:mt-10 lg:mt-30 p-2 text-center md:text-left">
+                    <h3 className="font-[MuseoModerno] font-bold text-3xl lg:text-4xl">
                         Learn to Code
                     </h3>
-                    <p className="p-1">
+                    <p className="p-2">
                         Jump into the world of programming with SyntaxRush — where coding
                         meets competition! Whether you're a beginner or a seasoned
                         developer, our platform gives you the tools to master languages like
                         Python, JavaScript, C++, and more.
                     </p>
-                    <div className="p-2 m-3">
+                    <div className="p-2 m-2">
                         <Link
                             to="/signup"
                             className="rounded-2xl gap-1.5 px-3 py-2.5 has-[>svg]:px-2.5 bg-blue-700 text-white hover:bg-blue-500">
@@ -27,12 +29,12 @@ export function UpperContainer() {
                 </div>
             </div>
 
-            <div className="grid grid-cols-1 mt-40 p-2 lg:grid-cols-2 md:grid-cols-2 sm: gap-4">
-                <div className="grid grid-cols-1 gap-4 p-2 mb-7">
-                    <h3 className="text-center text-2xl font-medium md:text-right">
+            <div className="grid grid-cols-1 gap-4 mt-40 p-2 md:grid-cols-2">
+                <div className="flex flex-col gap-4 p-2 lg:mt-20">
+                    <h3 className="font-[MuseoModerno] font-medium text-center md:text-right text-2xl lg:text-4xl">
                         Add Fun in Learning
                     </h3>
-                    <p className="p-1 text-justify md:text-right">
+                    <p className="p-2 text-justify md:text-right">
                         Who said coding has to be boring? At SyntaxRush, we’ve gamified
                         learning with real-time coding battles, leaderboard rankings, and
                         seasonal tournaments. Compete with friends or challengers worldwide
@@ -44,7 +46,9 @@ export function UpperContainer() {
                         {"Start Exploring \u003E"}
                     </Link>
                 </div>
-                <div className="w-2xs h-64 m-auto p-2"></div>
+                <div className="p-2">
+                    <img src={graphic2} alt="Add Fun in Learning Image" className="w-xs md:w-xl" />
+                </div>
             </div>
         </div>
     );
